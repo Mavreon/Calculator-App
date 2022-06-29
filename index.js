1d1a84a7c9a14ed490b4e53ad1ad072534082fea
Chisom Ogbonna <mavreon@gmail.com>
2022-06-29 21:40:00 +0100

    Added Basic Toggle Logic

---
+++ index.js
@@ -0,0 +1,31 @@
+const themeToggles = document.querySelectorAll('.theme-toggle');
+const indicator = document.querySelector('.indicator');
+console.log(themeToggles);
+console.log(indicator);
+
+themeToggles.forEach(themeToggle => {
+    themeToggle.addEventListener('click', ()=>{
+        // console.log(themeToggle + " Clicked");
+        CheckToggleState(themeToggle);
+    });
+});
+
+function CheckToggleState(toggleState){
+    switch(toggleState)
+    {
+        case themeToggles[0]:
+            console.log("Clicked First Theme");
+            indicator.style.transform = `translateX(-2.3rem)`;
+            break;
+
+        case themeToggles[1]:
+            console.log("Clicked Second Theme");
+            indicator.style.transform = `translateX(0rem)`;
+            break;
+
+        case themeToggles[2]:
+            console.log("Clicked Third Theme");
+            indicator.style.transform = `translateX(2.3rem)`;
+            break;
+    }
+}
