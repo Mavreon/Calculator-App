f1e3573db23f4a1e2a9a9b4a04c27cafa21553b3
Chisom Ogbonna <mavreon@gmail.com>
2022-06-30 19:56:39 +0100

    Final Updates

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const indicator = document.querySelectorAll('.indicator');
 const displaySection = document.querySelectorAll('.display-section');
 const keypadSection = document.querySelectorAll('.keypad-section');
 const keypadButtons = document.querySelectorAll('.keypad-btn');
-// console.log(themeToggles);
-// console.log(indicator);
-// console.log(keypadButtons);
-console.log(body);
-let themeState = "theme-one";
+const attribution = document.querySelectorAll('.attribution');
+const displayText = document.querySelector('.display-text');
 
+var themeState = "theme-one";
+var screenText = "";
+var action = "";
+var numbers = [];
+var result = 0;
+var pointOnce = true;
 
-ChangeAllTheme(themeState);
+ChangeAllElementsThemeState(themeState);
 
 themeToggles.forEach(themeToggle => {
     themeToggle.addEventListener('click', ()=>{
@@ -21,33 +24,203 @@ themeToggles.forEach(themeToggle => {
     });
 });
 
+keypadButtons.forEach(keypadButton => {
+    keypadButton.addEventListener('click',()=>{
+        //Call keypad function
+        KeypadFunction(keypadButton);
+    })
+});
+
+function KeypadFunction(keypad)
+{
+    var list = keypad.classList;
+    if(list.contains("delete-btn")){
+        screenText = screenText.slice(0, -1);
+        displayText.textContent = screenText;
+    }
+    else if(list.contains("point-btn"))
+    {
+        if(pointOnce && screenText.length > 0){
+            UpdateDisplayText(keypad.textContent);
+            pointOnce = false;
+        }
+    }
+    else if(list.contains("add-btn"))
+    {
+        action = "add";
+        pointOnce = true;
+        if(screenText.includes("/") || screenText.includes("-") || screenText.includes("x"))
+        {
+            ClearDisplayText();
+        }
+        else if(screenText[screenText.length-1] != "+" && screenText.length > 0)
+        {
+            UpdateDisplayText(keypad.textContent);
+        }
+    }
+    else if(list.contains("sub-btn"))
+    {
+        action = "sub";
+        pointOnce = true;
+        if( screenText.includes("/") || screenText.includes("+") || screenText.includes("x"))
+        {
+            ClearDisplayText();
+        }
+        else if(screenText[screenText.length-1] != "-"&& screenText.length > 0)
+        {
+            UpdateDisplayText(keypad.textContent);
+        }
+    }
+    else if(list.contains("div-btn"))
+    {
+        action = "div";
+        pointOnce = true;
+        if( screenText.includes("-") || screenText.includes("+") || screenText.includes("x"))
+        {
+            ClearDisplayText();
+        }
+        else if(screenText[screenText.length-1] != "/"&& screenText.length > 0)
+        {
+            UpdateDisplayText(keypad.textContent);
+        }
+    }
+    else if(list.contains("mult-btn"))
+    {
+        action = "mult";
+        pointOnce = true;
+        if( screenText.includes("/") || screenText.includes("+") || screenText.includes("-"))
+        {
+            ClearDisplayText();
+        }
+        else if(screenText[screenText.length-1] != "x"&& screenText.length > 0)
+        {
+            UpdateDisplayText(keypad.textContent);
+        }
+    }
+    else if(list.contains("reset-btn"))
+    {
+        ClearDisplayText();
+        pointOnce = true;
+        action = "";
+    }
+    else if(list.contains("equal-btn")){
+        if(screenText.includes("+") || screenText.includes("-") || screenText.includes("x") || screenText.includes("/"))
+        {
+            switch(action)
+            {
+                case "add":
+                    numbers = screenText.split("+");
+                    numbers.forEach(number => {
+                        result+=Number(number);
+                    });
+                    //console.log(numbers);
+                    break;
+                case "sub":
+                    numbers = screenText.split("-");
+                    console.log(numbers);
+                    numbers.forEach(number => {
+                        if(result == 0)
+                        {
+                            result=Number(number);
+                        }
+                        else{
+                            result-=Number(number);
+                        }
+                    });
+                    //console.log(numbers);
+                    break;
+                case "div":
+                    numbers = screenText.split("/");
+                    console.log(numbers);
+                    numbers.forEach(number => {
+                        if(result == 0)
+                        {
+                            result=Number(number);
+                        }
+                        else{
+                            result/=Number(number);
+                        }
+                    });
+                    //console.log(numbers);
+                    break;
+                case "mult":
+                    numbers = screenText.split("x");
+                    numbers.forEach(number => {
+                        if(result == 0)
+                        {
+                            result=Number(number);
+                        }
+                        else{
+                            result*=Number(number);
+                        }
+                    });
+                    //console.log(numbers);
+                    break;
+            }
+            
+            result.toFixed(2);
+            //console.log(result);
+            SetDisplayText(result);
+            screenText = result;
+            numbers = [];
+            //result = 0;
+        }
+       
+    }
+    else{
+        UpdateDisplayText(keypad.textContent);
+    }
+}
+
+function ClearDisplayText(){
+    screenText = "";
+    displayText.textContent = screenText;
+}
+
+function SetDisplayText(text){
+    screenText = text;
+    displayText.textContent = screenText;
+}
+
+function UpdateDisplayText(text){
+    if(result>0)
+    {
+        ClearDisplayText();
+        result = 0;
+    }
+    if(screenText.length<12){
+        screenText += text;
+        displayText.textContent = screenText;
+    }
+}
+
 function CheckToggleState(toggleState){
     switch(toggleState)
     {
         case themeToggles[0]:
-            console.log("Clicked First Theme");
+            //console.log("Clicked First Theme");
             indicator[0].style.transform = `translateX(-2rem)`;
             themeState = "theme-one";
-            ChangeAllTheme(themeState);
+            ChangeAllElementsThemeState(themeState);
             break;
 
         case themeToggles[1]:
-            console.log("Clicked Second Theme");
+            //console.log("Clicked Second Theme");
             indicator[0].style.transform = `translateX(0rem)`;
             themeState = "theme-two";
-            ChangeAllTheme(themeState);
+            ChangeAllElementsThemeState(themeState);
             break;
 
         case themeToggles[2]:
-            console.log("Clicked Third Theme");
+            //console.log("Clicked Third Theme");
             indicator[0].style.transform = `translateX(2rem)`;
             themeState = "theme-three";
-            ChangeAllTheme(themeState);
+            ChangeAllElementsThemeState(themeState);
             break;
     }
 }
 
-function ChangeTheme(themeState, elements)
+function ChangeElementTheme(themeState, elements)
 {
     elements.forEach( element => {
         const list = element.classList;
@@ -59,14 +232,15 @@ function ChangeTheme(themeState, elements)
     });
 }
 
-function ChangeAllTheme(themeState)
+function ChangeAllElementsThemeState(themeState)
 {
-    ChangeTheme(themeState, body);
-    ChangeTheme(themeState, headSection);
-    ChangeTheme(themeState, toggleThemeSwitch);
-    ChangeTheme(themeState, indicator);
-    ChangeTheme(themeState, displaySection);
-    ChangeTheme(themeState, keypadSection);
-    ChangeTheme(themeState, keypadButtons);
+    ChangeElementTheme(themeState, body);
+    ChangeElementTheme(themeState, headSection);
+    ChangeElementTheme(themeState, toggleThemeSwitch);
+    ChangeElementTheme(themeState, indicator);
+    ChangeElementTheme(themeState, displaySection);
+    ChangeElementTheme(themeState, keypadSection);
+    ChangeElementTheme(themeState, keypadButtons);
+    ChangeElementTheme(themeState, attribution);
 }
 
